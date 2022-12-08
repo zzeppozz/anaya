9dcadf2218ac306f4434c906279287d941cbb7bb
zzeppozz <zzeppozz@gmail.com>
2022-12-08 13:40:14 -0600

    Update constants.py
    Include verbatim coordinates in output csv and shapefile.

--- dammap/common/constants.py
+++ dammap/common/constants.py
@@ -76,10 +76,10 @@ SHP_FIELDS = [
     (IMAGE_KEYS.LON, ogr.OFTReal),
     (IMAGE_KEYS.LAT, ogr.OFTReal),
     (IMAGE_KEYS.WKT, ogr.OFTString),
-    # (IMAGE_KEYS.VERB_LON, ogr.OFTString),
-    # (IMAGE_KEYS.VERB_LAT, ogr.OFTString),
-    # (IMAGE_KEYS.VERB_LON_DIR, ogr.OFTString),
-    # (IMAGE_KEYS.VERB_LAT_DIR, ogr.OFTString),
+    (IMAGE_KEYS.VERB_LON, ogr.OFTString),
+    (IMAGE_KEYS.VERB_LAT, ogr.OFTString),
+    (IMAGE_KEYS.VERB_LON_DIR, ogr.OFTString),
+    (IMAGE_KEYS.VERB_LAT_DIR, ogr.OFTString),
     (IMAGE_KEYS.X_DIR, ogr.OFTString),
     (IMAGE_KEYS.X_DEG, ogr.OFTInteger),
     (IMAGE_KEYS.X_MIN, ogr.OFTInteger),
@@ -134,37 +134,5 @@ BAD_FILES = [
     "5_Cottonwood/cottonwood_2018-03-22_0028.JPG",
     "5_Cottonwood/cottonwood_2018-03-22_0005.JPG",
     "5_Cottonwood/cottonwood_2018-05-07_0002.JPG",
-]
-
-SOME_DUPES = [
-    [
-        "14_Liz/liz_2021-07-11_0671", "14_Liz/liz_2021-07-11_0681", "14_Liz/liz_2021-07-11_0691",
-        "14_Liz/liz_2021-07-11_0701", "14_Liz/liz_2021-07-11_0721", "14_Liz/liz_2021-07-11_0731",
-        "14_Liz/liz_2021-07-11_0741"
-        "17_Josie/josie_2021-08-01.0011", "17_Josie/josie_2021-08-01.0021", "17_Josie/josie_2021-08-01.0031",
-        "17_Josie/josie_2021-08-01.0041", "17_Josie/josie_2021-08-01.0051", "17_Josie/josie_2021-08-01.0061",
-        "17_Josie/josie_2021-08-01.0071", "17_Josie/josie_2021-08-01.0081", "17_Josie/josie_2021-08-01.0091",
-        "17_Josie/josie_2021-08-01.0101", "17_Josie/josie_2021-08-01.0111", "17_Josie/josie_2021-08-01.0121",
-        "17_Josie/josie_2021-08-01.0131", "17_Josie/josie_2021-08-01.0141", "17_Josie/josie_2021-08-01.0151",
-        "17_Josie/josie_2021-08-01.0161", "17_Josie/josie_2021-08-01.0171", "17_Josie/josie_2021-08-01.0181",
-        "17_Josie/josie_2021-08-01.0191",
-        "17_Josie/josie_2021-08-01.0201", "17_Josie/josie_2021-08-01.0211", "17_Josie/josie_2021-08-01.0231",
-        "17_Josie/josie_2021-08-01.0251", "17_Josie/josie_2021-08-01.0261", "17_Josie/josie_2021-08-01.0281",
-        "17_Josie/josie_2021-08-01.0291"
-        "46_Sauna/sauna_2021-07-16.0011"
-        "5_Cottonwood/cottonwood_2021-07-30_0041", "5_Cottonwood/cottonwood_2021-07-30_0051",
-        "5_Cottonwood/cottonwood_2021-07-30_0061", "5_Cottonwood/cottonwood_2021-07-30_0071",
-        "5_Cottonwood/cottonwood_2021-07-30_0081", "5_Cottonwood/cottonwood_2021-07-30_0091",
-        "5_Cottonwood/cottonwood_2021-07-30_0101"
-    ],
-    [
-        "16_Spillover/spillover_2014-11-22_024",
-        "18_Gerroyo/gerroyo_2014-11-23_0011", "18_Gerroyo/gerroyo_2014-11-23_0021",
-        "18_Gerroyo/gerroyo_2014-11-23_0031", "18_Gerroyo/gerroyo_2014-11-23_0071",
-        "18_Gerroyo/gerroyo_2014-11-23_0111", "18_Gerroyo/gerroyo_2014-11-23_0141",
-        "18_Gerroyo/gerroyo_2014-11-23_0151", "18_Gerroyo/gerroyo_2014-11-23_0161",
-        "18_Gerroyo/gerroyo_2014-11-23_0171", "18_Gerroyo/gerroyo_2014-11-23_0181"
-    ]
-
 ]
 
